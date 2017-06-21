b0cf0b602bcdd12b7b3edea25fc42aa76b00dbfc
Rodrigo Luger <rodluger@gmail.com>
2017-06-20 18:36:48 -0700

    misc improvements

--- group1.py
+++ group1.py
@@ -13,8 +13,17 @@ star = tools.Load(ID)
 star.search(periods = np.linspace(5., 20., 1000), 
             phases = np.linspace(0., 1., 1000))
 
-# Once you know the phase and the period, compute the depth posterior
-# depth, depth_uncert = star.compute_depth(phase = 0., per = 10.)
+# Once you know the phase and the period, enter them here:
+phase = None
+per = None
 
-# Once you know the depth, plot the folded light curve!
-# star.plot_folded(phase = 0., per = 10., depth = depth)
+if not (phase is None) and not (per is None):
+
+  # Once you know the phase and the period, compute the depth posterior
+  depth, depth_uncert = star.compute_depth(phase = phase, per = per)
+
+  # Once you know the depth, plot the folded light curve!
+  star.plot_folded(phase = phase, per = per, depth = depth)
+
+  # Print the result
+  print("Depth = %.6f +/- %.6f" % (depth, depth_uncert))
--- group2.py
+++ group2.py
@@ -14,8 +14,17 @@ star.search(joint_fit = True,
             periods = np.linspace(5., 20., 1000), 
             phases = np.linspace(0., 1., 1000))
 
-# Once you know the phase and the period, compute the depth posterior
-# depth, depth_uncert = star.compute_depth(joint_fit = True, phase = 0., per = 10.)
+# Once you know the phase and the period, enter them here:
+phase = None
+per = None
 
-# Once you know the depth, plot the folded light curve!
-# star.plot_folded(phase = 0., per = 10., depth = depth)
+if not (phase is None) and not (per is None):
+
+  # Once you know the phase and the period, compute the depth posterior
+  depth, depth_uncert = star.compute_depth(joint_fit = True, phase = phase, per = per)
+
+  # Once you know the depth, plot the folded light curve!
+  star.plot_folded(phase = phase, per = per, depth = depth)
+
+  # Print the result
+  print("Depth = %.6f +/- %.6f" % (depth, depth_uncert))
